52d68655eb1bddd108ba15a632015bebd97963e2
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-01 23:17:19 +0200

    feat: add responsive text for country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import './Country.css';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-export const Country = ({ name }) => (
+export const Country = ({ name }) => {
+  const stats = useSelector(state => state.covidApi);
+  const details = stats.currentInfo.filter(i => i.location === name)[0] === undefined ? { active: '-' } : stats.currentInfo.filter(i => i.location === name)[0];
 
-  <div>
-    <Card border="dark" className="my-2" style={{ width: '18rem' }}>
-      <Card.Header>{name}</Card.Header>
-      <Card.Body>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk
-          of the card content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-);
+  const detailsLink = {
+    pathname: `country/${name}`,
+    state: details,
+  };
+  return (
+    <div>
+      <Link to={detailsLink}>
+        <Card border="dark" className="my-2 country-card" style={{ width: '18rem' }}>
+          <Card.Header>{name}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <span>
+                Active:
+                {' '}
+                {details.active}
+              </span>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
+    </div>
+  );
+};
 
 export default Country;
