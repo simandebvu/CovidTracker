2e42fd02d18420986a146ae563b3436aaee3dc20
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-10-04 17:53:41 +0200

    feat: add tweet functionality

--- src/components/CountryDetail.jsx
+++ src/components/CountryDetail.jsx
@@ -5,14 +5,27 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { ImEarth } from 'react-icons/im';
 
 export const CountryDetail = ({ match, location }) => {
   const curName = match.params.countryname;
+  const {
+    confirmed, deaths, recovered, active,
+  } = location.state;
+  const openURL = url => {
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+  };
+
+  const handleShareTwitter = () => {
+    openURL(`https://twitter.com/intent/tweet?hashtags=covid19&related=hiresimandebvu&text=${encodeURIComponent(`Stay Home. Stay Safe. ${curName}. Confirmed Cases : ${confirmed}! `)}`);
+  };
+
   return (
     <div className="my-5">
       <Jumbotron className="container">
         <Container>
-          <h1>
+          <h1 className="text-secondary">
+            <span className="text-primary"><ImEarth /></span>
             {' '}
             {curName}
           </h1>
@@ -27,17 +40,17 @@ export const CountryDetail = ({ match, location }) => {
             </thead>
             <tbody>
               <tr>
-                <td>{location.state.confirmed}</td>
-                <td>{location.state.deaths}</td>
-                <td>{location.state.recovered}</td>
-                <td>{location.state.active}</td>
+                <td className="text-warning font-weight-bold">{confirmed}</td>
+                <td className="text-danger font-weight-bold">{deaths}</td>
+                <td className="text-success font-weight-bold">{recovered}</td>
+                <td className="text-primary font-weight-bold">{active}</td>
               </tr>
             </tbody>
           </Table>
 
-          <Button variant="warning" className="text-white"><Link to="/">Back</Link></Button>
+          <Button variant="warning" className="btn-danger"><Link to="/"><span className="text-white">Back</span></Link></Button>
           {' '}
-          <Button variant="primary">Tweet</Button>
+          <Button variant="primary" onClick={handleShareTwitter}>Tweet</Button>
           {' '}
         </Container>
       </Jumbotron>
